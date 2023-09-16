f64202bb77bf390c61c0e8e5536078a8acb26a70
alshareef49 <alshareef4039@gmail.com>
2023-09-16 22:01:59 +0530

    added logback jacoco and customer feature

--- src/main/java/com/ecommerce/repository/CartProductRepository.java
+++ src/main/java/com/ecommerce/repository/CartProductRepository.java
@@ -3,7 +3,5 @@ package com.ecommerce.repository;
 import com.ecommerce.entity.CartProduct;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface CartProductRepository extends CrudRepository<CartProduct,Integer> {
 }
